a4f93595d394a94d4c9bcb47a8741bcecf1394f9
kscold <chan6502@gmail.com>
2023-08-29 20:55:50 +0900

    [승찬] "정문 페이지 없데이트"

--- frontend/src/components/container/InformationPage/door.js
+++ frontend/src/components/container/InformationPage/door.js
@@ -1,12 +1,92 @@
-// src/components/container/InformationPage/door.js
+// // src/components/container/InformationPage/door.js
+// import React, { useState } from "react"
+// import image from "./image.png"
+// import "./door.scss"
+// import Header from "../../common/Header"
+
+// const Door = () => {
+//   const [selectedTags, setSelectedTags] = useState([])
+//   const tags = ["인터스트리디자인", "소프트웨어", "수학", "과학"]
+//   const [isGateSelected, setIsGateSelected] = useState(false)
+
+//   const toggleGateSelection = () => {
+//     setIsGateSelected((prev) => !prev)
+//   }
+
+//   const onClickTag = (tag) => {
+//     setSelectedTags((prevTags) => {
+//       if (prevTags.includes(tag)) {
+//         return prevTags.filter((t) => t !== tag)
+//       } else if (prevTags.length < 3) {
+//         return [...prevTags, tag]
+//       } else {
+//         return [tag, ...prevTags.slice(0, 2)]
+//       }
+//     })
+//   }
+
+//   return (
+//     <>
+//       <Header />
+//       <div className="navigation-cp-container">
+//         <div className="navigation-cp-space">
+//           <div className="navigation-cp">디자인대학</div>
+//           <div
+//             className={`navigation-cp${isGateSelected ? ".active" : ""}`}
+//             onClick={toggleGateSelection}
+//           >
+//             정문
+//           </div>
+//         </div>
+//       </div>
+//       <div className={`navigation-line ${isGateSelected ? "active" : ""}`} />
+
+//       <div>
+//         <div className="image-size">
+//           <img
+//             src={image}
+//             alt="이미지"
+//             style={{
+//               width: "363px",
+//               height: "175px",
+//             }}
+//           />
+//         </div>
+
+//         <div className="filter-tags">
+//           {tags.map((tag) => (
+//             <button
+//               key={tag}
+//               className={`tag-button ${
+//                 selectedTags.includes(tag) ? "active" : ""
+//               }`}
+//               onClick={() => onClickTag(tag)}
+//             >
+//               #{tag}
+//             </button>
+//           ))}
+//         </div>
+//       </div>
+//     </>
+//   )
+// }
+
+// export default Door
+
+// Door.js
 import React, { useState } from "react"
 import image from "./image.png"
-import "./door.scss" // 상대 경로를 명시하고 .scss 확장자를 붙이지 않음
+import "./door.scss"
 import Header from "../../common/Header"
+import tags from "./tags.json"
 
 const Door = () => {
   const [selectedTags, setSelectedTags] = useState([])
-  const tags = ["인터스트리디자인", "소프트웨어", "수학", "과학"] // 하드코딩한 태그 리스트
+  const [isGateSelected, setIsGateSelected] = useState(false)
+
+  const toggleGateSelection = () => {
+    setIsGateSelected((prev) => !prev)
+  }
 
   const onClickTag = (tag) => {
     setSelectedTags((prevTags) => {
@@ -23,6 +103,21 @@ const Door = () => {
   return (
     <>
       <Header />
+      <div className="navigation-cp-container">
+        <div className="navigation-cp-space">
+          <div className="navigation-cp">디자인대학</div>
+          <div
+            className={`navigation-cp ${isGateSelected ? "active" : ""}`}
+            onClick={toggleGateSelection}
+          >
+            정문
+          </div>
+        </div>
+      </div>
+      <div className={`navigation-line ${isGateSelected ? "active" : ""}`}>
+        {isGateSelected && <div className="selected"></div>}
+      </div>
+
       <div>
         <div className="image-size">
           <img
@@ -35,18 +130,21 @@ const Door = () => {
           />
         </div>
 
-        <div className="filter-tags">
-          {tags.map((tag) => (
-            <button
-              key={tag}
-              className={`tag-button ${
-                selectedTags.includes(tag) ? "active" : ""
-              }`}
-              onClick={() => onClickTag(tag)}
-            >
-              #{tag}
-            </button>
-          ))}
+        <div className="filter-container">
+          <h2 className="filter-title">동아리/학과 부스</h2>
+          <div className="filter-tags">
+            {tags.map((tag) => (
+              <button
+                key={tag}
+                className={`tag-button ${
+                  selectedTags.includes(tag) ? "active" : ""
+                }`}
+                onClick={() => onClickTag(tag)}
+              >
+                {tag}
+              </button>
+            ))}
+          </div>
         </div>
       </div>
     </>
