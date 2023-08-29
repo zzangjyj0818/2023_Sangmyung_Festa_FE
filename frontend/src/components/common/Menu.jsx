27c74851de365b06f3e55322f3b215c36048d01b
zzangjyj0818 <jyj010818@naver.com>
2023-08-29 19:21:40 +0900

    Edit Side Menu bar

--- frontend/src/components/common/Menu.jsx
+++ frontend/src/components/common/Menu.jsx
@@ -6,8 +6,12 @@ const Menu = ({isOpen, setIsOpen ,onClick}) => {
         <div className="Modal" onClick={onClick}>
             <div className="modalBody" onClick={(e) => e.stopPropagation()}>
                 <button id="modalCloseBtn" onClick={onClick}>
-                    ✖
+                    x
                 </button>
+                <div className='Modal_Text'>
+                    <div className='Modal_Text1'>Dear For U</div>
+                    <div className='Modal_Text2'>: 비상</div>
+                </div>
             </div>
         </div>
     );
