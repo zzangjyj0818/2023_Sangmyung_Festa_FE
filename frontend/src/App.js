09cfd7b522d1503666d0bcacc6bb4bc1deb51982
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2023-08-29 16:04:52 +0900

    [HyungJun ] 111

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import InformationPage from './pages/information/InformationPage';// information.jsx 파일을 임포트하세요
 
 const App = () => {
   return (
-    <div>
-      app
-    </div>
+    <InformationPage/>
   );
 };
 
