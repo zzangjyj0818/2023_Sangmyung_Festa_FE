af1e17f3637220884b1915198d33f76bff116152
zzangjyj0818 <jyj010818@naver.com>
2023-08-29 20:12:52 +0900

    [YeonJae] Implement about Side Menu Bar

--- frontend/src/components/common/Menu.jsx
+++ frontend/src/components/common/Menu.jsx
@@ -1,6 +1,24 @@
 import React from 'react';
 import '../../styles/components/common/Menu.scss';
 
+const items = [
+    {
+        id : 1,
+        text : '🎆 주요 행사',
+    },
+    {
+        id : 2,
+        text : '⏰ 타임 테이블',
+    },
+    {
+        id : 3,
+        text : '🎉 부스/푸드트럭',
+    },
+    {
+        id : 4,
+        text : '🤷 누가 만들었나요?',
+    }
+]
 const Menu = ({isOpen, setIsOpen ,onClick}) => {
     return (
         <div className="Modal" onClick={onClick}>
@@ -11,6 +29,11 @@ const Menu = ({isOpen, setIsOpen ,onClick}) => {
                 <div className='Modal_Text'>
                     <div className='Modal_Text1'>Dear For U</div>
                     <div className='Modal_Text2'>: 비상</div>
+                </div>
+                <div className='Modal_Horizon'></div>
+                <div className='Modal_items'>
+                    {items.map(item => 
+                        <div key={item.id} className='Modal_item'>{item.text}</div>)}
                 </div>
             </div>
         </div>
