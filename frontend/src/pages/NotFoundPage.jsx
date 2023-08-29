a588d69fcb74ec22241db717aad2fa777b5db170
zzangjyj0818 <jyj010818@naver.com>
2023-08-29 15:28:19 +0900

    [YeonJae] design the NotFoundPage

--- frontend/src/pages/NotFoundPage.jsx
+++ frontend/src/pages/NotFoundPage.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import '../styles/pages/NotFoundPage.scss';
 
 const NotFoundPage = () => {
     return (
-        <div>
-            404 Not Found
+        <div className='NotFoundPage_Container'>
+            <div className='NotFoundPage_Text'>
+                요청하신 페이지를 찾을 수 없습니다.
+            </div>
         </div>
     );
 };
