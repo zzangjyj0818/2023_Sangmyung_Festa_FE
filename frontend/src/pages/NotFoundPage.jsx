3cd1915949f76b94f9b0d85fba2bbb756f481b13
zzangjyj0818 <jyj010818@naver.com>
2023-08-29 15:40:11 +0900

    Finish the NotFoundPage

--- frontend/src/pages/NotFoundPage.jsx
+++ frontend/src/pages/NotFoundPage.jsx
@@ -4,8 +4,10 @@ import '../styles/pages/NotFoundPage.scss';
 const NotFoundPage = () => {
     return (
         <div className='NotFoundPage_Container'>
-            <div className='NotFoundPage_Text'>
-                요청하신 페이지를 찾을 수 없습니다.
+            <div className='NotFoundPage_Wrapper'>
+                <div className='NotFoundPage_Text'>
+                    요청하신 페이지를 찾을 수 없습니다.
+                </div>
             </div>
         </div>
     );
