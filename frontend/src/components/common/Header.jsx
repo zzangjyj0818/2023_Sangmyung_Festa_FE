6f4d2703ef3320c1b9122a2cec7369a8fd453f30
zzangjyj0818 <jyj010818@naver.com>
2023-08-29 19:08:57 +0900

    Implement the Side Menu Bar

--- frontend/src/components/common/Header.jsx
+++ frontend/src/components/common/Header.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
+import Menu from './Menu';
 import Logo from '../../assets/Logo.png';
 import '../../styles/components/common/Header.scss';
 import { AiOutlineMenu } from "react-icons/ai";
 
-const Header = () => {
+const Header = ({isOpen, setIsOpen, onClick}) => {
     return (
         <div className='Header_Container'>
             <div className='Header_Wrapper'>
                 <div className='Header_Img'>
                     <img src = {Logo} alt='' style={{width: '67px', height : '48px'}}/>
                 </div>
-                <div className='Header_MenuIcon'>
+                <div className='Header_MenuIcon' onClick={onClick}>
                     <AiOutlineMenu size={20}/>
+                    {isOpen && <Menu isOpen = {isOpen} setIsOpen = {setIsOpen} onClick={onClick} />}
                 </div>
             </div>
         </div>
