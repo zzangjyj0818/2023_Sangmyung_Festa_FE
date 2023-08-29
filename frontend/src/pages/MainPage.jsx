cdecc428c3a9a238392adf63d2597656d7d2570f
jm7033 <113967528+jm7033@users.noreply.github.com>
2023-08-29 19:47:26 +0900

    jimin11111

--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../components/common/Header';
+import MainView from '../components/presentation/MainPage/MainView';
 
 const MainPage = () => {
     return (
         <div>
             <Header />
+            <MainView />
         </div>
     );
 };
