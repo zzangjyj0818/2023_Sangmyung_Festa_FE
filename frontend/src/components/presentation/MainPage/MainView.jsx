bf49615df8b0bd8b36af1e87e4d944714cd12826
zzangjyj0818 <jyj010818@naver.com>
2023-08-30 22:07:43 +0900

    .

--- frontend/src/components/presentation/MainPage/MainView.jsx
+++ frontend/src/components/presentation/MainPage/MainView.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import TextBox from '../../../assets/TextBox.png';
 import '../../../styles/components/MainPage/MainView.scss';
 
 const MainView = () => {
     return (
         <div className='MainView_Container'>
-            <div>
+            <div className='MainView_TextBox'>
                 <div>2023.9/25-26</div>
                 <div>DEAR FOR YOU</div>
                 <div>: 비상</div>
