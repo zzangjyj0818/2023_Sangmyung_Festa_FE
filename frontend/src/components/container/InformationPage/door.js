07ba064392a3627bdd8c23e748cd0444fc3c7a1e
kscold <chan6502@gmail.com>
2023-08-30 21:55:13 +0900

    [승찬] "임형준 봐라"

--- frontend/src/components/container/InformationPage/door.js
+++ frontend/src/components/container/InformationPage/door.js
@@ -2,8 +2,9 @@
 import React, { useState } from "react"
 import image from "./image.png"
 import "./door.scss"
-import Header from "../../common/Header"
 import tags from "./tags.json"
+import timeicon from "./carbon_time.png"
+import infoicon from "./Vector.png"
 
 const Door = () => {
   const [selectedTag, setSelectedTag] = useState("")
@@ -24,7 +25,7 @@ const Door = () => {
   }
 
   return (
-    <>
+    <div className="informaion-container">
       <div className="navigation-cp-container">
         <div className="navigation-cp-space">
           <button
@@ -51,6 +52,73 @@ const Door = () => {
       <div className="body-container">
         <div className="image-size">
           <img src={image} alt="이미지" />
+          <div className="overlay-div-container">
+            <div
+              className={`tag-div1 ${
+                selectedTag.includes("인터스트리디자인") ? "active" : ""
+              }`}
+            />
+
+            <div
+              className={`tag-div2 ${
+                selectedTag.includes("소프트웨어") ? "active" : ""
+              }`}
+            />
+
+            <div
+              className={`tag-div3 ${
+                selectedTag.includes("팜므") ? "active" : ""
+              }`}
+            />
+
+            <div
+              className={`tag-div4 ${
+                selectedTag.includes("스페이스디자인") ? "active" : ""
+              }`}
+            />
+
+            <div
+              className={`tag-div5 ${
+                selectedTag.includes("마음건강연구회") ? "active" : ""
+              }`}
+            />
+
+            <div
+              className={`tag-div6 ${
+                selectedTag.includes("그린스마트시티") ? "active" : ""
+              }`}
+            />
+
+            <div
+              className={`tag-div7 ${
+                selectedTag.includes("시스템반도체") ? "active" : ""
+              }`}
+            />
+
+            <div
+              className={`tag-div8 ${
+                selectedTag.includes("단과대") ? "active" : ""
+              }`}
+            />
+
+            <div
+              className={`tag-div9 ${
+                selectedTag.includes("무대미술") ? "active" : ""
+              }`}
+            />
+
+            <div
+              className={`tag-div10 ${
+                selectedTag.includes("식물식품공학") ? "active" : ""
+              }`}
+            />
+
+            <div
+              className={`tag-div11 ${
+                selectedTag.includes("세라믹디자인") ? "active" : ""
+              }`}
+            />
+          </div>
         </div>
 
         <div className="filter-container">
@@ -69,8 +137,26 @@ const Door = () => {
             ))}
           </div>
         </div>
+
+        <div className="timeline-container">
+          <div className="timeline-title">
+            <div className="timeline-icon">
+              <img src={timeicon} alt="시계 아이콘" />
+            </div>
+            <h2 className="timeline-icon-font">시간</h2>
+            <h2 className="timeline-time">10:00 ~ 20:00</h2>
+          </div>
+          <div className="timeline-component">
+            <div className="timeline-body" />
+            <div className="timeline-icon">
+              <img src={timeicon} alt="시계 아이콘" />
+            </div>
+            <h2 className="timeline-cp-title">게임 및 사진촬영</h2>
+            <h2 className="timeline-cp-cost">1,000원 / 10발</h2>
+          </div>
+        </div>
       </div>
-    </>
+    </div>
   )
 }
 
