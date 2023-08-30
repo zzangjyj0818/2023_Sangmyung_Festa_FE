2ae284c2d76fd71f97275b46ad4e88e92648064f
zzangjyj0818 <jyj010818@naver.com>
2023-08-31 00:20:16 +0900

    [YeonJae] Working About Reponsive Styling

--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -3,6 +3,7 @@ import HeaderContainer from '../components/container/common/HeaderContainer';
 import MainView from '../components/presentation/MainPage/MainView';
 import EventView from '../components/presentation/MainPage/EventView';
 import TimelineView from '../components/presentation/MainPage/TimelineView';
+import CreatorView from '../components/presentation/MainPage/CreatorView';
 import Footer from '../components/common/Footer';
 import '../styles/pages/MainPage.scss';
 
@@ -11,10 +12,13 @@ const MainPage = () => {
         <>
             <HeaderContainer name = {undefined} type = {2} opacity={true}/>
             <MainView/>
-            <div>
+            <div style={{height:'100vh'}}>
                 <EventView/>
                 <TimelineView />
-            </div> 
+            </div>
+            <div style={{height:'100vh'}}>
+                <CreatorView />
+            </div>
             <Footer /> 
         </>
     );
