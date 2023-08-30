720b9249760f1867b2b1a75426ed27fc82958b7b
jm7033 <113967528+jm7033@users.noreply.github.com>
2023-08-30 17:51:46 +0900

    miss

--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import HeaderContainer from '../components/container/common/HeaderContainer';
 import MainView from '../components/presentation/MainPage/MainView';
+import Footer from '../components/common/Footer';
 
 const MainPage = () => {
     return (
         <>
             <HeaderContainer />
             <MainView/>
+            <Footer/>
         </>
     );
 };
