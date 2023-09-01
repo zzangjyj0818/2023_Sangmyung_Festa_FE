b318ef8674953227b72764e4aba2bf0d567e84ae
zzangjyj0818 <jyj010818@naver.com>
2023-09-02 04:21:43 +0900

    commit

--- frontend/src/components/presentation/MainPage/EventView.jsx
+++ frontend/src/components/presentation/MainPage/EventView.jsx
@@ -1,3 +1,4 @@
+/*EventView.jsx*/
 import React from 'react';
 import AirplaneSeat from '../../../assets/MainPage/AirplaneSeat.png';
 import EventGift from '../../../assets/MainPage/EventGift.png';
@@ -6,12 +7,10 @@ import '../../../styles/components/MainPage/EventView.scss';
 
 const detailInformations = [
     {
-        id : 1,
-        url : undefined,
+        url : 'https://www.youtube.com/watch?v=jOTfBlKSQYY',
         boolDirection: false,
     },
     {
-        id : 2,
         title : '🛩️ 스위치 비행기 좌석 추첨',
         first : '탑승권을 구매하고 다양한 경품을',
         second : '받아가보세요! 이곳은 최대 두 줄입니다',
@@ -19,7 +18,6 @@ const detailInformations = [
         url : AirplaneSeat
     },
     {
-        id : 3,
         title : '🎁 Switch의 선물을 unlock!',
         first : '상명대에 숨겨진 암호쪽지를 찾고',
         second : '보물을 차지하세요!',
@@ -27,37 +25,50 @@ const detailInformations = [
         url : EventGift
     },
     {
-        id : 4,
         title : '👕 드레스코드',
         first : '드레스코드를 맞춰 입고',
         second : '경품도 받아가세요!',
-        boolDirection : true,
-        url : DressCode
-    },
+        boolDirection: true,
+       url: DressCode
+   }
 ]
 
-const EventBox = ({detail}) => (
-    <div className="event_box">
-        <img src={detail.url} alt='' style={{width : '287px', height : '195px'}}/>
-        <div></div>
-    </div>
-);
-
-const EventView = () => {
+const EventBox = ({detail}) => {
+    const isYoutubeLink = detail.url && detail.url.includes('youtube.com/watch?v=');
+    const thumbnailUrl = isYoutubeLink 
+        ? `https://img.youtube.com/vi/${detail.url.split('v=')[1]}/0.jpg`
+        : detail.url;
+ 
     return (
-        <div className='EventView_Container'>
-            <div className="Event_Text">
-                <p>📢 이벤트</p>
+        <div className="event_box">
+            <a href={detail.url} target="_blank" rel="noopener noreferrer">
+                <img src={thumbnailUrl} alt=''/>
+            </a>
+            <div className="text_detail">
+                {detail.title && <p className="title_detail">{detail.title}</p>}
+                {detail.first && <p className="first_detail">{detail.first}</p>}
+                {detail.second && <p className="second_detail">{detail.second}</p>}
             </div>
-            <div className='EventView_Wrapper'>
-                <div className='EventView_Slide'>
-                    <div className="scroll-container">
-                        {detailInformations.map((detail) => <EventBox detail={detail} key = {detail.id}/>)}
-                    </div>
-                </div>
-            </div>
+            {detail.boolDirection && <div className="arrow_detail">{`>`}</div>}
         </div>
     );
+};
+
+const EventView = () => {
+   return (
+       <div className='EventView_Container'>
+           <div className="Event_Text">
+               <p>📢 이벤트</p>
+           </div>
+           <div className='EventView_Wrapper'>
+               <div className='EventView_Slide'>
+                   <div className="scroll-container">
+                       {detailInformations.map((detail, index) => (<EventBox key={index} detail={detail} />))}
+                   </div>
+               </div>
+           </div>
+       </div>
+   );
 };
 
 export default EventView;
