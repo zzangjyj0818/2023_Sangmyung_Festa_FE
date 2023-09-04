3978d2bdcff2a51d7570e2bcbe9cd82d164b3da0
zzangjyj0818 <jyj010818@naver.com>
2023-09-04 11:51:34 +0900

    [YeonJae] Implement the TimelinePage about Event

--- frontend/src/components/presentation/TimelinePage/ComponentAssembly.jsx
+++ frontend/src/components/presentation/TimelinePage/ComponentAssembly.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import '../../../styles/components/TimelinePage/ComponentAssembly.scss'
 import TableType from './TableType';
-import SlideShow from './SlideShow';
 
 const ComponentAssembly = () => {
     return (
         <div className='ComponentAssembly_Container'>
             <div className='ComponentAssembly_Wrapper'>
                 <TableType/>
-                <SlideShow/>
             </div>
         </div>
     );
