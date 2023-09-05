d8a2ff8f3f351f1a9f89434765e49f494c315b0b
zzangjyj0818 <jyj010818@naver.com>
2023-09-05 23:28:58 +0900

    Edit the SlideShow (SCSS)

--- frontend/src/components/presentation/TimelinePage/SlideShow.jsx
+++ frontend/src/components/presentation/TimelinePage/SlideShow.jsx
@@ -3,7 +3,7 @@ import '../../../styles/components/TimelinePage/SlideShow.scss';
 import AirPlane from '../../../assets/MainPage/AirplaneSeat.png';
 
 const SlideShow = () => {
-    const sliderRef = useRef(null);
+  const sliderRef = useRef(null);
 
   useEffect(() => {
     if (!sliderRef.current) return;
@@ -11,52 +11,85 @@ const SlideShow = () => {
     const handleScroll = () => {
       const slider = sliderRef.current;
       const slides = Array.from(slider.children);
-      let activeSlideIndex = slides.findIndex(slide => slide.classList.contains('active'));
+    
+      const currentScroll = slider.scrollLeft + slider.offsetWidth / 2;
       
-      const currentScroll = slider.scrollLeft;
-      const slideStarts = slides.map(slide => slide.offsetLeft);
-
-      for(let i=0; i<slideStarts.length; i++) {
-        if(currentScroll + slider.offsetWidth/2 <= slideStarts[i]) { 
-          activeSlideIndex = i-1; 
-          break;
-        }
-
-        if(i === slideStarts.length -1) activeSlideIndex = i;
-      }
-
+      let closestSlideIndex = slides.reduce((prev, curr, index) => {
+        return Math.abs(curr.offsetLeft + curr.offsetWidth / 2 - currentScroll) < Math.abs(slides[prev].offsetLeft + slides[prev].offsetWidth / 2 - currentScroll)
+          ? index
+          : prev;
+      }, 0);      
+      
       slides.forEach((slide, index) => {
-        slide.classList.remove('active', 'blur');
-        if(index === activeSlideIndex) slide.classList.add('active');
-        else slide.classList.add('blur');
+        if (index === closestSlideIndex) {
+          slide.classList.add("active");
+          slide.classList.remove("blur");
+        } else {
+          slide.classList.add("blur");
+          slide.classList.remove("active");
+        }
       });
     };
 
     // 스크롤 이벤트 리스너 등록
-    sliderRef.current.addEventListener('scroll', handleScroll);
+    sliderRef.current.addEventListener("scroll", handleScroll);
 
     // 컴포넌트 unmount 시에 이벤트 리스너 제거
     return () => {
-       if (sliderRef.current) { // 해당 DOM 요소의 존재 여부 확인 후 제거 로직 실행
-         sliderRef.current.removeEventListener('scroll', handleScroll);
-       }
+      if (sliderRef.current) {
+        // 해당 DOM 요소의 존재 여부 확인 후 제거 로직 실행
+        sliderRef.current.removeEventListener("scroll", handleScroll);
+      }
     };
-  }, [sliderRef.current]);
+  }, []); // sliderRef.current를 의존성 배열에서 제거
+
+
+  const handleClickImage = (event, index) => {
+    event.preventDefault();
+  
+    if (!sliderRef.current || !sliderRef.current.children[index]) return;
+  
+    // Calculate the scroll position of the clicked image and scroll there smoothly
+    let newScrollPosition =
+      sliderRef.current.children[index].offsetLeft -
+      (sliderRef.current.offsetWidth - sliderRef.current.children[index].offsetWidth) / 2;
+  
+     // Scroll to the clicked image smoothly within the container only
+     smoothScroll(newScrollPosition - 12);
+  };
+  
+
+
+
+function smoothScroll(targetPosition){
+     let start=null;
+     let currentPosition=sliderRef?.current?.scrollLeft ||0;
      
+     function step(timestamp){
+         if(!start) start=timestamp;
+         let progress=timestamp-start;
+         sliderRef.current.scrollLeft=currentPosition+(targetPosition-currentPosition)*progress/500;
+         if(progress<495) window.requestAnimationFrame(step);
+        
+     }
+     window.requestAnimationFrame(step);
+ }
+
+
   return (
-   <div className='slider_Container'>
+   <div className='slide_Container'>
      <div className="slider" ref={sliderRef}>
-       <div className="slide active" style={{marginLeft : '57px'}}>
-         <img src={AirPlane} alt="Image 1"/>
+       <div className="slide active" onClick={(event) => handleClickImage(event, 0)} style={{marginLeft : '41px'}}>
+         <img src={AirPlane} alt=""/>
        </div>
-       <div className="slide blur"> 
-         <img src={AirPlane} alt="Image 2"/>
+       <div className="slide blur" onClick={(event) => handleClickImage(event, 1)}>
+         <img src={AirPlane} alt=""/>
        </div>
-       <div className="slide blur">
-         <img src={AirPlane} alt="Image 3"/>
+       <div className="slide blur" onClick={(event) => handleClickImage(event, 2)}>
+         <img src={AirPlane} alt=""/>
        </div>
-       <div className="slide blur">
-         <img src={AirPlane} alt ="Image4"/>
+       <div className="slide blur" onClick={(event) => handleClickImage(event, 3)} style={{marginRight : '41px'}}>
+         <img src={AirPlane} alt =""/>
        </div>
      </div>
    </div>
