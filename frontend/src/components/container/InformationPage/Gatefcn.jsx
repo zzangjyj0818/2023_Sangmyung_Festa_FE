ecf5419e0464c750fd4a4acca1dfcf16c141a58c
kscold <chan6502@gmail.com>
2023-09-13 18:44:02 +0900

    [SeungChan] "코드 스플리팅만 하면 되는 부스 코드 업데이트

--- frontend/src/components/container/InformationPage/Gatefcn.jsx
+++ frontend/src/components/container/InformationPage/Gatefcn.jsx
@@ -1,27 +1,79 @@
 // src/components/container/InformationPage/Gatefcn.jsx
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import DragInfo from "./drag" // DragInfo import
-import { tagData_food, tagData2 } from "./data"
+import { tagData2, tagData_food2 } from "./data"
 // scss 임포트
 import "./informationfcn.scss"
 import "./Gatefcn.scss"
 
-const GateFcn = ({ onClickTag }) => {
+const TagList = ({ tags, onTagClick, selectedTagId }) => {
+  return (
+    <div className="filter-tags">
+      {tags.map((tag) => (
+        <button
+          key={tag.id}
+          className={`tag-button ${selectedTagId === tag.id ? "active" : ""}`}
+          onClick={() => onTagClick(tag.id)}
+        >
+          {tag.name}
+        </button>
+      ))}
+    </div>
+  )
+}
+
+function GateFcn() {
   const [selectedTagId, setSelectedTagId] = useState(null)
-  const [selectedFoodTruckId, setSelectedFoodTruckId] = useState(null) // 추가
+  const [selectedTagId_food, setSelectedTagId_food] = useState(null)
+  const [showMajorTags, setShowMajorTags] = useState(false)
+  const [favorites, setFavorites] = useState([]) // favorites state 추가
 
-  const handleClick = (name, id) => {
-    onClickTag(name)
-
-    setSelectedTagId(id)
-    setSelectedFoodTruckId(null) // 다른 태그 선택 시 해당 태그 초기화
+  const handleFavorite = (selectedTagContent) => {
+    if (favorites.includes(selectedTagContent)) {
+      setFavorites((prevFavorites) =>
+        prevFavorites.filter((favorite) => favorite !== selectedTagContent)
+      )
+    } else {
+      setFavorites((prevFavorites) => [...prevFavorites, selectedTagContent])
+    }
   }
 
-  const handleFoodTruckClick = (name, id) => {
-    onClickTag(name)
-    setSelectedFoodTruckId(id)
-    setSelectedTagId(null) // 다른 태그 선택 시 해당 태그 초기화
+  const handleTagClick = (tagId) => {
+    setSelectedTagId(tagId)
+    setSelectedTagId_food(null)
   }
+
+  const handleTagFood = (tagId) => {
+    setSelectedTagId_food(tagId)
+    setSelectedTagId(null)
+  }
+
+  const handleTouchStart = (e) => {
+    e.preventDefault()
+  }
+
+  const handleTouchMove = (e) => {
+    e.preventDefault()
+  }
+
+  useEffect(() => {
+    const handleStorageChange = (e) => {
+      if (e.key === "favorites2") {
+        setFavorites(JSON.parse(e.newValue))
+      }
+    }
+
+    window.addEventListener("storage", handleStorageChange)
+
+    return () => {
+      window.removeEventListener("storage", handleStorageChange)
+    }
+  }, [])
+
+  useEffect(() => {
+    const storedFavorites = JSON.parse(localStorage.getItem("favorites2")) || []
+    setFavorites(storedFavorites)
+  }, [])
 
   return (
     <>
@@ -48,7 +100,7 @@ const GateFcn = ({ onClickTag }) => {
 
         <div
           className={`gate-booth foodbooth ${
-            selectedFoodTruckId ? "active" : ""
+            selectedTagId_food ? "active" : ""
           }`}
         >
           <span className="building-name">푸드트럭</span>
@@ -64,45 +116,82 @@ const GateFcn = ({ onClickTag }) => {
         ))}
       </div>
 
-      <div className="filter-container">
-        <h2 className="filter-title">동아리/학과 부스</h2>
-
-        <div className="filter-tags">
-          {tagData2.map((tag) => (
-            <button
-              key={tag.id}
-              className={`tag-button ${
-                selectedTagId === tag.id ? "active" : ""
-              }`}
-              onClick={() => handleClick(tag.name, tag.id)}
-            >
-              {tag.name}
-            </button>
-          ))}
+      <div className="filters-layout-container">
+        <div
+          className="filter-container"
+          style={{ overflowX: "scroll", flexWrap: "nowrap" }}
+        >
+          <h2 className="filter-title">즐겨찾는 부스</h2>
+          <div className="filter-favorite-tags">
+            {favorites.length > 0 ? (
+              favorites.map((favorite) => (
+                <button
+                  className="favorite-tag-button"
+                  key={favorite}
+                  onClick={() => {
+                    // 일치하는 tagData 아이템 찾기
+                    const matchingTag = tagData2.find(
+                      (tag) => tag.name === favorite
+                    )
+                    if (matchingTag) {
+                      setSelectedTagId(matchingTag.id)
+                      setSelectedTagId_food(null) // 다른 태그 선택 시 해당 태그 초기화
+                    } else {
+                      // 일치하는 tagData_food 아이템 찾기
+                      const matchingFoodTag = tagData_food2.find(
+                        (tag) => tag.name === favorite
+                      )
+                      if (matchingFoodTag) {
+                        setSelectedTagId_food(matchingFoodTag.id)
+                        setSelectedTagId(null) // 다른 태그 선택 시 해당 태그 초기화
+                      }
+                    }
+                  }}
+                >
+                  {favorite}
+                </button>
+              ))
+            ) : (
+              <p className="DesignUnivFavorite">하트를 눌러 추가해주세요</p>
+            )}
+          </div>
         </div>
-      </div>
 
-      <div className="filter-container">
-        <h2 className="filter-title">푸트트럭</h2>
+        <div className="filter-container">
+          <div className="filter-title-container">
+            <h2 className="filter-title">동아리/학과 부스</h2>
+          </div>
+          {showMajorTags ? (
+            <TagList
+              tags={tagData2}
+              onTagClick={handleTagClick}
+              selectedTagId={selectedTagId}
+            />
+          ) : (
+            <TagList
+              tags={tagData2.slice(0, 8)}
+              onTagClick={handleTagClick}
+              selectedTagId={selectedTagId}
+            />
+          )}
+        </div>
 
-        <div className="filter-tags">
-          {tagData_food.map((truck) => (
-            <button
-              key={truck.id}
-              className={`tag-button ${
-                selectedFoodTruckId === truck.id ? "active" : ""
-              }`}
-              onClick={() => handleFoodTruckClick(truck.name, truck.id)}
-            >
-              {truck.name}
-            </button>
-          ))}
+        <div className="filter-container">
+          <h2 className="filter-title">푸드트럭</h2>
+          <TagList
+            tags={tagData_food2}
+            onTagClick={handleTagFood}
+            selectedTagId={selectedTagId_food}
+          />
         </div>
       </div>
 
       <DragInfo
         selectedTagId2={selectedTagId}
-        selectedTagId_food={selectedFoodTruckId}
+        selectedTagId_food2={selectedTagId_food}
+        onTouchStart={handleTouchStart}
+        onTouchMove={handleTouchMove}
+        onFavoriteChange={handleFavorite}
       />
     </>
   )
