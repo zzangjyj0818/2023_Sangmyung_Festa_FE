cf7b5b71e43287c9d55d0da87ab68753dd66026f
zzangjyj0818 <jyj010818@naver.com>
2023-09-15 01:59:02 +0900

    .

---
+++ frontend/src/components/presentation/TimelinePage/SmallBox.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import '../../../styles/components/TimelinePage/SmallBox.scss';
+import ColorTime from '../../../assets/TimelinePage/color_time.webp';
+import Location from '../../../assets/TimelinePage/carbon_location.webp';
+
+const SmallBox = ({tableData}) => {
+    const {content, time, location} = tableData;
+    return (
+        <div className='SmallBox_Container'>
+            <div className='SmallBox_TextBox'>
+                <div className='SmallBox_MainTitle'>{content}</div>
+                <div className='SmallBox_DisplaySet'>
+                    <div style={{marginRight : '10px'}}>
+                        <div className="Time_Icon">{<img src={ColorTime} alt=''/>}</div>
+                        <div className='Information_time'>{time}</div>
+                    </div>
+                    <div>
+                        <div className="Location_Icon"><img src={Location} alt=''/></div>
+                        <div className='Information_SubTitle'>{location}</div>
+                    </div>
+                </div>
+            </div>
+        </div> 
+    );
+};
+
+export default SmallBox;
