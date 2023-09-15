16f5f68f8086622b40d65b9fc3da3794c58253c8
zzangjyj0818 <jyj010818@naver.com>
2023-09-15 17:59:32 +0900

    commit

--- frontend/src/components/presentation/TimelinePage/Boxes/Box.jsx
+++ frontend/src/components/presentation/TimelinePage/Boxes/Box.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../../../../styles/components/TimelinePage/Boxes/Box.scss';
-import ColorTime from '../../../../assets/TimelinePage/color_time.svg';
-import Location from '../../../../assets/TimelinePage/carbon_location.svg';
+import ColorTime from '../../../../assets/TimelinePage/color_time.webp';
+import Location from '../../../../assets/TimelinePage/carbon_location.webp';
 
 const Box = ({tableData, boxType}) => {
     const {content, time, location} = tableData;
