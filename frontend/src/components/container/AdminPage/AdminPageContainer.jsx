8517149075fbe8397e839b3f8a7186e25bf09d2e
zzangjyj0818 <jyj010818@naver.com>
2023-09-17 20:23:51 +0900

    Complete the SlideShow

--- frontend/src/components/container/AdminPage/AdminPageContainer.jsx
+++ frontend/src/components/container/AdminPage/AdminPageContainer.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import AdminPage from '../../../pages/AdminPage';
 import { fetchAll } from '../../../api/fetchAll';
 import { useQuery } from 'react-query';
@@ -6,8 +6,8 @@ import { useQuery } from 'react-query';
 const AdminPageContainer = () => {
     const [isOpen, setIsOpen] = useState(true);
     const { data: all, refetch } = useQuery('allState', fetchAll, {
-        staleTime: 1000 * 60 * 0.05 // 캐시된 데이터가 5분 동안 신선한 상태로 유지됩니다.
-      });
+        staleTime: 1000 * 60 * 0.5,
+    });
 
     return (
         <AdminPage 
--- frontend/src/components/container/TimelinePage/SlideShowContainer.jsx
+++ frontend/src/components/container/TimelinePage/SlideShowContainer.jsx
@@ -1,19 +1,33 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import SlideShow from '../../presentation/TimelinePage/SlideShow';
+import SwiperCore from 'swiper';
 
 const SlideShowContainer = ({datas, selectedDate}) => {
     const [index, setIndex] = useState(0);
+    const swiperRef = useRef(null); // Ref to access Swiper instance
 
    const params = {
-      on: { slideChange: index => { 
-          setIndex(index.snapIndex);
-       }},
+      on: { 
+          init: (swiper) => {
+              swiperRef.current = swiper; // Save Swiper instance to ref
+          },
+          slideChange: (swiper) => { 
+              setIndex(swiper.activeIndex);
+          }
+      },
       spaceBetween: 170,
       slidesPerView: 2,
       centeredSlides: true,
       roundLengths: true,
       loop: false,
    }
+
+   useEffect(() => {
+       if (selectedDate && swiperRef.current) {
+           setIndex(0);
+           swiperRef.current.slideTo(0); // Change the current slide to the first one
+       }
+   }, [selectedDate]);
 
    return (
      <SlideShow 
