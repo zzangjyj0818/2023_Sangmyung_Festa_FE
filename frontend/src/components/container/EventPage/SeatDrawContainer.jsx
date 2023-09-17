95ae482a1e64ab880c302adbf2ec2370117750d6
zzangjyj0818 <jyj010818@naver.com>
2023-09-17 20:53:11 +0900

    Edit css

--- frontend/src/components/container/EventPage/SeatDrawContainer.jsx
+++ frontend/src/components/container/EventPage/SeatDrawContainer.jsx
@@ -5,7 +5,9 @@ import SeatDraw from '../../presentation/EventPage/SeatDraw';
 
 const SeatDrawContainer = () => {
     const { data: ticket, refetch} = useQuery('ticket', fetchTicket, {
-        staleTime: 1000 * 60 * 0.05 // 캐시된 데이터가 5분 동안 신선한 상태로 유지됩니다.
+        staleTime: 1000 * 60 * 1, // 캐시된 데이터가 5분 동안 신선한 상태로 유지됩니다.
+        refetchOnWindowFocus: false,
+        retry: false,
       });
 
     let month = new Date().getMonth()+1;
