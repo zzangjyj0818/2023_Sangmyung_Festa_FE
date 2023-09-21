2ce2cfa8bac7265494d5c51d14341f4e030361b1
zzangjyj0818 <jyj010818@naver.com>
2023-09-21 20:24:37 +0900

    .

--- frontend/src/api/fetchDressCode.jsx
+++ frontend/src/api/fetchDressCode.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const fetchDressCode = async () => {
     try {
-      const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/event/dress`);
+      const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/admin/festival/api/get_dress`);
       return response.data; // 데이터는 response.data 안에 들어있습니다.
     } catch (e) {
       console.error(e);
--- frontend/src/api/fetchTicket.jsx
+++ frontend/src/api/fetchTicket.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const fetchTicket = async () => {
     try {
-      const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/event/ticket`);
+      const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/admin/festival/api/get_ticket`);
       console.log(response.data);
       return response.data.ticket; // 데이터는 response.data 안에 들어있습니다.
     } catch (e) {
