8edafaf21e8f33f095c4d63436774402666ea036
zzangjyj0818 <jyj010818@naver.com>
2023-09-28 21:12:37 +0900

    .

--- frontend/src/api/postBoxState.jsx
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-export const postBoxState = async ({idx}) => {
-  const response = await axios.patch(`${process.env.REACT_APP_SERVER_URL}/admin/festival/api/box_toggle/${idx}`, {});
-  return response.data.response;
-};
